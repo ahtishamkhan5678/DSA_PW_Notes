3bb0ba30b1eab2a6c8dd3c08088207c615ce2743
Mohammad Ahtisham <100846744+ahtishamkhan5678@users.noreply.github.com>
2023-06-05 11:45:30 +0530

    Update Leetcode problem -237.java

--- LinkedList/Leetcode problem -237.java
+++ LinkedList/Leetcode problem -237.java
@@ -1,1 +1,15 @@
+// **Q1→  Can we delete a node given the  node itself as parameter? (Leetcode  problem - 237)**
 
+//   **- Is there any efficient way, provided that the given node is not the last node?**
+
+// **Sol → approach :** 
+
+// example : 13→1→100→5→20→null;
+
+// given parameter is 5 ok so,
+
+// what we do i.e
+
+// [a.data](http://a.data)(parameter ke ander) = a.next.data;(agle wle value store kr do); // 13→1→100→20→null;
+
+// [a.next](http://a.next) = a.next.next;
